b2855a647bb307300a00ac26d5da8f7c3b65b24f
Jeremy Rimpo <jrim@rimpo.org>
2025-12-24 12:09:15 -0600

    Tag release 1.4.0

--- src/ExploData/explo_data/const.py
+++ src/ExploData/explo_data/const.py
@@ -4,5 +4,5 @@
 # Licensed under the [GNU Public License (GPL)](http://www.gnu.org/licenses/gpl-2.0.html) version 2 or later.
 
 plugin_name: str = 'ExploData'
-plugin_version: str = '1.4.0-alpha1'
+plugin_version: str = '1.4.0'
 database_version: int = 10
